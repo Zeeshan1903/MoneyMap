1be28ace96e427687d84342bc37fc4aa91fa6280
zeeshan <rangrejroshanara@gmail.com>
2024-10-23 22:12:47 +0530

    minor changes

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # MoneyMap
+Run the : "./run.sh" command in your machine after Clonining it 
--- run.sh
+++ run.sh
@@ -3,6 +3,8 @@ echo "Months : Jan, Feb, Apr, Mar, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec"
 rm -rf a.out 
 
 rm -rf output.txt
+
+#In place of PhonePe_Statement_Jul2024_Oct2024.pdf write your phonpe transaction detail pdf name  
 pdftotext -layout PhonePe_Statement_Jul2024_Oct2024.pdf output.txt 
 g++ main.cpp 
 ./a.out 
